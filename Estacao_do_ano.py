d07c6ad3efab867bdf232176e96074fa9f379184
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-18 16:59:08 -0300

    -Commit Thais linda

--- Estacao_do_ano.py
+++ Estacao_do_ano.py
@@ -3,22 +3,34 @@
 #Apresentação
 print('\n\t\t\t -- Estação do ano -- \n\n\t')
 
-#Entrada
-dia=int(input('Informe o dia: '))
-mes=int(input('Informe o mês: '))
+#Entradas
+dia=int(input('Informe o dia:\n '))
+mes=int(input('Informe o mês:\n '))
 
 #Processamento e Saída
-if mes==12 and dia>=22 or mes==1 or mes==2 or mes==3 and dia<20:
-    print(f'Verão dia {dia} de{mes} de 2023')
+if mes==1 or mes==2 or mes==3:
+    if mes == 3 and dia >= 20:
+        print('\n Outono \n')
+    else:
+        print('Verão')
 
-elif mes==3 and dia >=20 or mes==4 or mes==5 or mes==6 and dia<21:
-    print(f'Outono dia: {dia} de {mes} de 2023')
+elif mes==4 or mes==5 or mes==6:
+    if mes == 6 and dia >= 21:
+        print('Inverno')
+    else:
+        print('Outono')
 
-elif mes==6 and dia >=21 or mes==7 or mes==8 or mes==9 and dia<23:
-    print(f'Inverno dia {dia} de {mes} de 2023')
+elif mes==7 or mes==8 or mes==9:
+    if mes == 9 and dia >=23:
+        print('Primavera')
+    else:
+        print('Inverno')
 
-elif mes==9 and dia >=23 or mes==10 or mes==11 or mes==12 and dia <22:
-    print(f'Primavera dia {dia} de {mes} de 2023')
+elif  mes==10 or mes==11 or mes==12:
+    if mes==12 and dia >=22:
+        print('Verão')
+    else:
+        print(f'Primavera')
 
 else:
     print('Mês inválido')
